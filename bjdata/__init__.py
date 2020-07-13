2f280cc4788cb065523c9e8b382601bf53825d19
Qianqian Fang <fangqq@gmail.com>
2020-07-12 22:31:41 -0400

    tag v0.2.5

--- bjdata/__init__.py
+++ bjdata/__init__.py
@@ -38,6 +38,6 @@ except ImportError:  # pragma: no cover
 from .encoder import EncoderException
 from .decoder import DecoderException
 
-__version__ = '0.2.9'
+__version__ = '0.2.5'
 
 __all__ = ('EXTENSION_ENABLED', 'dump', 'dumpb', 'EncoderException', 'load', 'loadb', 'DecoderException')
